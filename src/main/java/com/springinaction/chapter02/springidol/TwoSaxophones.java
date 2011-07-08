3b17d4c22d54409a87384a8d6472fdb501be8cbe
Stefano Locati <stefano@obliquid.it>
2011-07-08 14:10:42 +0800

    Demonstrating inner beans

---
+++ src/main/java/com/springinaction/chapter02/springidol/TwoSaxophones.java
@@ -0,0 +1,15 @@
+package com.springinaction.chapter02.springidol;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TwoSaxophones {
+
+    public static void main(String[] args) throws PerformanceException {
+        String config = "com/springinaction/chapter02/springidol/two-saxophones.xml";
+        ApplicationContext appContext = new ClassPathXmlApplicationContext(config);
+        ((Performer) appContext.getBean("kenny")).perform();
+        ((Performer) appContext.getBean("duke")).perform();
+    }
+
+}
